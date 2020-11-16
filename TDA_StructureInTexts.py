c1a4e93a448ff27366792107120af06a4432c837
Matt O'Reilly <fermoymatt@gmail.com>
2020-11-16 21:39:42 +0000

    Functions
    Adapted code into functions

--- TDA_StructureInTexts.py
+++ TDA_StructureInTexts.py
@@ -12,12 +12,23 @@ import math  # math fun
 import matplotlib.pyplot as plt  # plotting
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-from ripser import ripser  # persistent homology package
+from ripser import ripser, Rips  # persistent homology package
 from persim import plot_diagrams, bottleneck  # analyzing Persistence Diagrams
+import sklearn
+
+########################################################
+########################################################
+############                        ####################
+############      Text File         ####################
+############                        ####################
+########################################################
+########################################################
 
 
-a_file = open("./texts/DylanThomas.txt")  # sample.txt
-lines = a_file.readlines()
+a_file = open("./texts/sample.txt")  # DylanThomas/sample.txt
+Sample = a_file.readlines()
+a_file = open("./texts/DylanThomas.txt")  # DylanThomas/sample.txt
+DylanThomas = a_file.readlines()
 
 ########################################################
 ########################################################
@@ -28,16 +39,15 @@ lines = a_file.readlines()
 ########################################################
 
 
-CountVec = CountVectorizer(ngram_range=(0, 1))
-Count_data = CountVec.fit_transform(lines)
-
-# create dataframe
-cv_dataframe = pd.DataFrame(
-    Count_data.toarray(), columns=CountVec.get_feature_names())
-print(cv_dataframe)
-
-data = np.array(cv_dataframe)  # Array of length 4 (In this case)
-takens_vector = np.concatenate(data)  # Convert Array to single vector.
+def wourd_count(data):
+    CountVec = CountVectorizer(ngram_range=(0, 1))
+    Count_data = CountVec.fit_transform(data)
+    cv_dataframe = pd.DataFrame(
+        Count_data.toarray(), columns=CountVec.get_feature_names())
+    # Convert Array to single vector.
+    global takens_vector
+    takens_vector = np.concatenate(np.array(cv_dataframe))
+    return takens_vector
 
 ########################################################
 ########################################################
@@ -52,6 +62,7 @@ def takensEmbedding(data, delay, dimension):
     "This function returns the Takens embedding of data with delay into dimension, delay*dimension must be < len(data)"
     if delay * dimension > len(data):
         raise NameError('Delay times dimension exceed length of data!')
+        global embeddedData
     embeddedData = np.array([data[0:len(data) - delay * dimension]])
     for i in range(1, dimension):
         embeddedData = np.append(
@@ -59,24 +70,30 @@ def takensEmbedding(data, delay, dimension):
     return embeddedData
 
 
-# Embedded into 2 Dimensions
-embedded_data2 = takensEmbedding(takens_vector, 1, 2)
-# Plot into 2D
-fig = plt.figure()
-# plot the 2D embedding
-ax = fig.add_subplot(3, 1, 2)
-ax.plot(embedded_data2[0, :], embedded_data2[1, :]);
-plt.show()
-
-
-# Embedded into 3 Dimensions
-embedded_data3 = takensEmbedding(takens_vector, 1, 3)
-# Plot into 3D
-fig = plt.figure()
-# plot the 3D embedding
-ax = fig.add_subplot(3, 1, 2, projection='3d')
-ax.plot(embedded_data3[0, :], embedded_data3[1, :], embedded_data3[2, :]);
-plt.show()
+def plot_embedding(embeddedData, dimension):
+    if dimension == 2:
+        # Embedded into 2 Dimensions
+        global embedding_2d
+        embedding_2d = embeddedData
+        # Plot into 2D
+        fig = plt.figure()
+        # plot the 2D embedding
+        ax = fig.add_subplot(3, 1, dimension)
+        ax.plot(embedding_2d[0, :], embedding_2d[1, :]);
+        plt.show()
+        return embedding_2d
+    if dimension == 3:
+        # Embedded into 3 Dimensions
+        global embedding_3d
+        embedding_3d = embeddedData
+        # Plot into 3D
+        fig = plt.figure()
+        # plot the 3D embedding
+        ax = fig.add_subplot(3, 1, 3, projection='3d')
+        ax.plot(embedding_3d[0, :],
+                embedding_3d[1, :], embedding_3d[2, :]);
+        plt.show()
+        return embedding_3d
 
 
 ########################################################
@@ -87,8 +104,26 @@ plt.show()
 ########################################################
 ########################################################
 
-diagrams = ripser(np.transpose(embedded_data3))['dgms']
-plot_diagrams(diagrams, show=True)
+def plot_persistence_diagram(embedding_dim):
+    if embedding_dim == 2:
+        diagrams = ripser(embedding_2d)['dgms']
+        plot_diagrams(diagrams, show=True)
+    if embedding_dim == 3:
+        diagrams = ripser(embedding_3d)['dgms']
+        plot_diagrams(diagrams, show=True)
 
 
 #persim.bottleneck(dgm1, dgm2, matching=False)
+
+# Dylan Thomas
+wourd_count(DylanThomas)
+takensEmbedding(takens_vector, 1, 3)
+plot_embedding(embeddedData, 3)
+plot_persistence_diagram(3)
+
+
+# Sample Text
+wourd_count(Sample)
+takensEmbedding(takens_vector, 1, 3)
+plot_embedding(embeddedData, 3)
+plot_persistence_diagram(3)
