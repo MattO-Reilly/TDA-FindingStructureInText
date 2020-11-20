5f447ffb2cf931654a68a23f9d5078a9cf539654
Matt O'Reilly <fermoymatt@gmail.com>
2020-11-20 21:41:02 +0000

    Update TDA_StructureInTexts.py

--- TDA_StructureInTexts.py
+++ TDA_StructureInTexts.py
@@ -10,27 +10,62 @@ import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import math  # math fun
 import matplotlib.pyplot as plt  # plotting
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
+# Persistent Homology
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from ripser import ripser, Rips  # persistent homology package
 from persim import plot_diagrams, bottleneck  # analyzing Persistence Diagrams
 import sklearn
+
+import xml.etree.ElementTree as ET  # Get around for a parsing error
 import gudhi
 
+# NLP /Text standardization
+import nltk
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from collections import Counter
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+import string
+
+from pathlib import Path
 ########################################################
 ########################################################
 ############                        ####################
-############      Text File         ####################
+############       Text File        ####################
 ############                        ####################
 ########################################################
 ########################################################
 
+
+# NLTK for word processing
+
+'''
+def open_file(path):
+    root = ET.fromstring(("./texts/" + str(path)))
+    root = tree.getroot()
+    a_file = open(root).lower()
+    path = a_file.readlines()
+    return path
+
+
+
+def open_file(text_file):
+    data_folder = Path("./texts/")
+    file_to_open = str(data_folder) + str(text_file)
+    root = ET.fromstring(file_to_open)
+    f = open(root).lower()
+    text_file = f_file.readlines()
+
+'''
 
 a_file = open(("./texts/sample.txt").lower())  # DylanThomas/sample.txt
 Sample = a_file.readlines()
 a_file = open(("./texts/DylanThomas.txt").lower())  # DylanThomas/sample.txt
 DylanThomas = a_file.readlines()
-
+a_file = open(("./texts/Hemingway.txt").lower())  # DylanThomas/sample.txt
+Hemingway = a_file.readlines()
 
 ########################################################
 ########################################################
@@ -41,9 +76,21 @@ DylanThomas = a_file.readlines()
 ########################################################
 
 
-def wourd_count(data):
+def standardize_text(text):
+    global std_text
+    input_str = str(text)
+    # result = input_str.translate(string.punctuation)
+    words = nltk.word_tokenize(input_str)
+    words = [word.lower() for word in words if word.isalpha()]
+    stop_words = set(stopwords.words("english"))
+    std_text = [i for i in words if not i in stop_words]
+    print(std_text)
+    return std_text
+
+
+def wourd_count(std_text):
     CountVec = CountVectorizer(ngram_range=(0, 1))
-    Count_data = CountVec.fit_transform(data)
+    Count_data = CountVec.fit_transform(std_text)
     cv_dataframe = pd.DataFrame(
         Count_data.toarray(), columns=CountVec.get_feature_names())
     # Convert Array to single vector.
@@ -115,19 +162,31 @@ def plot_persistence_diagram(embedding_dim):
         plot_diagrams(diagrams, show=True)
 
 
-#persim.bottleneck(dgm1, dgm2, matching=False)
+# persim.bottleneck(dgm1, dgm2, matching=False)
+
 
 # Dylan Thomas
-wourd_count(DylanThomas)
+print("DylanThomas")
+standardize_text(DylanThomas)
+wourd_count(std_text)
 takensEmbedding(takens_vector, 1, 3)
 plot_embedding(embeddedData, 3)
 plot_persistence_diagram(3)
 
 
 # Sample Text
-wourd_count(Sample)
+print("Sample text")
+standardize_text(Sample)
+wourd_count(std_text)
 takensEmbedding(takens_vector, 1, 3)
 plot_embedding(embeddedData, 3)
 plot_persistence_diagram(3)
 
-#gudhi.plot_persistence_barcode(embedding_3d)
+# Hemingway Text
+print("Hemingway")
+standardize_text(Hemingway)
+wourd_count(std_text)
+takensEmbedding(takens_vector, 1, 3)
+plot_embedding(embeddedData, 3)
+plot_persistence_diagram(3)
+# gudhi.plot_persistence_barcode(embedding_3d)
